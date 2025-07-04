09403f2544872c1f00098757d7811f00d3638f68
mohammad amani <141722054+mohammad76amani@users.noreply.github.com>
2025-07-05 02:24:57 +0330

    Add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import {Hero} from "../app/components/static/hero";
+import { Gallery } from "./components/static/gallery";
+
+export default function Home() {
+  return (
+    <div>
+    <Hero />
+    <Gallery />
+    </div>
+  );
+}
