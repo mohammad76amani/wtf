06734559422bed37c96d78ae5e6972b7c943f132
mohammad amani <141722054+mohammad76amani@users.noreply.github.com>
2025-07-05 02:24:42 +0330

    Add app/dashboard/page.tsx

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReserveArtist from '../components/artist-components/reserve-artist'
+
+const Page = () => {
+  return (
+    <div>
+        <h1>Dashboard</h1>
+
+        <ReserveArtist />
+    </div>
+  )
+}
+
+export default Page
